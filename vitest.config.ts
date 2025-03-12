fa843da03caef6c503b8c3fdd6a45c6f7c43ff96
nana4rider <register.develop@nana4.net>
2025-03-12 12:53:08 +0900

    feat: add coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   test: {
     globals: true,
     setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      include: ["src/**/*.ts"],
+      exclude: ["src/index.ts"],
+    },
   },
   plugins: [tsconfigPaths()],
 });
